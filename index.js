e0f728d269440784185d75709ffe673087796d6d
Joel Denning <joeldenning@gmail.com>
2020-01-27 19:48:16 -0700

    Skipping when no release is needed

--- index.js
+++ index.js
@@ -3,19 +3,36 @@ const Plugin = require('release-it/lib/plugin/Plugin.js')
 const packageJson = require(path.join(process.cwd(), './package.json'))
 
 module.exports = class CustomVersion extends Plugin {
-  getIncrementedVersionCI() {
-    if (!packageJson.name) {
-      throw Error(`package.json must have named with format '@esm-bundle/name'`)
+  static disablePlugin() {
+    const depVersion = getDepVersion()
+    const currentPackageVersion = packageJson.version
+
+    if (currentPackageVersion.startsWith(depVersion)) {
+      return ['git', 'github', 'npm', 'version']
+    } else {
+      return []
     }
-
-    const depName = packageJson.name.replace('@esm-bundle/', '') 
-    const dependency = (packageJson.dependencies || {})[depName]
-    if (!dependency) {
-      throw Error(`Missing package.json dependency '${depName}'`)
-    }
-
-    const number = /[0-9.]+/.exec(dependency)[0]
-
-    return number
   }
+  getIncrementedVersionCI() {
+    return getDepVersion()
+  }
+}
+
+function getDepName() {
+  if (!packageJson.name) {
+    throw Error(`package.json must have named with format '@esm-bundle/name'`)
+  }
+
+  return packageJson.name.replace('@esm-bundle/', '') 
+}
+
+function getDepVersion() {
+  const depName = getDepName()
+  const dependency = (packageJson.dependencies || {})[depName]
+  if (!dependency) {
+    throw Error(`Missing package.json dependency '${depName}'`)
+  }
+
+  return /[0-9.]+/.exec(dependency)[0]
+
 }
