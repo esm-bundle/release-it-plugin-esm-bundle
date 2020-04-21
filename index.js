2d2b7cc20f4ad3b009e739d862855d4b4e03ce3a
Joel Denning <joeldenning@gmail.com>
2020-04-21 17:49:07 -0600

    Support for scoped packages. Prettier

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-const path = require('path')
-const Plugin = require('release-it/lib/plugin/Plugin.js')
-const packageJson = require(path.join(process.cwd(), './package.json'))
+const path = require("path");
+const Plugin = require("release-it/lib/plugin/Plugin.js");
+const packageJson = require(path.join(process.cwd(), "./package.json"));
 
 module.exports = class CustomVersion extends Plugin {
   static disablePlugin() {
-    const depVersion = getDepVersion()
-    const currentPackageVersion = packageJson.version
+    const depVersion = getDepVersion();
+    const currentPackageVersion = packageJson.version;
 
     if (currentPackageVersion.startsWith(depVersion)) {
-      console.info("Disabling release-it because this version is already published")
-      return ['git', 'github', 'npm', 'version']
+      console.info(
+        "Disabling release-it because this version is already published"
+      );
+      return ["git", "github", "npm", "version"];
     } else {
-      return []
+      return [];
     }
   }
   getIncrementedVersionCI() {
-    return getDepVersion()
+    return getDepVersion();
   }
-}
+};
 
 function getDepName() {
   if (!packageJson.name) {
-    throw Error(`package.json must have named with format '@esm-bundle/name'`)
+    throw Error(`package.json must have named with format '@esm-bundle/name'`);
   }
 
-  return packageJson.name.replace('@esm-bundle/', '') 
+  const withoutPrefix = packageJson.name.replace("@esm-bundle/", "");
+  const [scope, name] = withoutPrefix.split("__");
+  if (name) {
+    // a scoped package
+    return `@${scope}/${name}`;
+  } else {
+    return scope;
+  }
 }
 
 function getDepVersion() {
-  const depName = getDepName()
-  const dependency = (packageJson.devDependencies || {})[depName]
+  const depName = getDepName();
+  const dependency = (packageJson.devDependencies || {})[depName];
   if (!dependency) {
-    throw Error(`Missing package.json dependency '${depName}'`)
+    throw Error(`Missing package.json dependency '${depName}'`);
   }
 
-  return /[0-9.]+/.exec(dependency)[0]
-
+  return /[0-9.]+/.exec(dependency)[0];
 }
